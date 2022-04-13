7e9f6f934123a0dff4400b7573adee500d5eac6a
Nannaleigh <tatianna.martinez71@gmail.com>
2022-04-13 11:20:48 -0600

    Working on productReducer/ redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-
+import "../src/index.css";
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
+import Home from "./components/Home";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <div className="container container-fluid">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
